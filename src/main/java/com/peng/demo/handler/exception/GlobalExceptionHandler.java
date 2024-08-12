95e1ca7fd2b00f645ec1d2a8e8358b2187655e42
bigpeng <1015792002@qq.com>
2024-08-12 19:48:30 +0800

    commit

--- src/main/java/com/peng/demo/handler/exception/GlobalExceptionHandler.java
+++ src/main/java/com/peng/demo/handler/exception/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.peng.demo.handler.exception;
 
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends RuntimeException {
+
+    private String code;
+    private String msg;
+
+    public GlobalExceptionHandler(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
