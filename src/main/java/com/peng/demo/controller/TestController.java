608ad91f4192c28e7cf99cc3fcd0f02d898e9ee9
bigpeng <1015792002@qq.com>
2024-08-12 20:12:48 +0800

    commit

--- src/main/java/com/peng/demo/controller/TestController.java
+++ src/main/java/com/peng/demo/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.peng.demo.controller;
 
 import com.peng.demo.handler.exception.GlobalExceptionHandler;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class TestController {
 
     @RequestMapping("/home")
     public String home() throws Exception {
-        throw new GlobalExceptionHandler("101","Global Error 错误");
+        throw new GlobalExceptionHandler("101", "Global Error 错误");
     }
 }
