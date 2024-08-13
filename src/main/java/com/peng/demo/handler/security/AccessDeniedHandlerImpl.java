83078abe10ef78944fe76f88e256575745bddca1
bigpeng <1015792002@qq.com>
2024-08-13 20:43:11 +0800

    commit

--- src/main/java/com/peng/demo/handler/security/AccessDeniedHandlerImpl.java
+++ src/main/java/com/peng/demo/handler/security/AccessDeniedHandlerImpl.java
@@ -1,4 +1,24 @@
 package com.peng.demo.handler.security;
 
-public class AccessDeniedHandlerImpl {
+import com.alibaba.fastjson.JSON;
+import com.peng.demo.common.ResponseResult;
+import com.peng.demo.util.WebUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AccessDeniedHandlerImpl implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ResponseResult result = new ResponseResult(HttpStatus.UNAUTHORIZED.value(), "认证失败请重新登录");
+        String json = JSON.toJSONString(result);
+        WebUtil.renderString(response,json);
+    }
 }
