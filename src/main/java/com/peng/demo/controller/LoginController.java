9685b0ed4b1f0b3a0b2107dc537f9836ea614e8d
bigpeng <1015792002@qq.com>
2024-08-13 20:07:58 +0800

    commit

--- src/main/java/com/peng/demo/controller/LoginController.java
+++ src/main/java/com/peng/demo/controller/LoginController.java
@@ -1,8 +1,21 @@
 package com.peng.demo.controller;
 
+import com.peng.demo.common.ResponseResult;
+import com.peng.demo.domain.entity.User;
+import com.peng.demo.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
+    @Autowired
+    private LoginService loginServcie;
+
+    @PostMapping("/user/login")
+    public ResponseResult login(@RequestBody User user){
+        return loginServcie.login(user);
+    }
 }
--- src/main/java/com/peng/demo/service/impl/LoginServiceImpl.java
+++ src/main/java/com/peng/demo/service/impl/LoginServiceImpl.java
@@ -10,10 +10,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Objects;
 
+@Service
 public class LoginServiceImpl implements LoginService {
 
     @Autowired
