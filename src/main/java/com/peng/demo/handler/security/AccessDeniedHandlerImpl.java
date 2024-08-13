8c1f47d5ed1ab46612e1f0cc15d60bbb1fb81a48
bigpeng <1015792002@qq.com>
2024-08-13 20:41:17 +0800

    commit

---
+++ src/main/java/com/peng/demo/handler/security/AccessDeniedHandlerImpl.java
@@ -0,0 +1,4 @@
+package com.peng.demo.handler.security;
+
+public class AccessDeniedHandlerImpl {
+}
---
+++ src/main/java/com/peng/demo/handler/security/AuthenticationEntryPointImpl.java
@@ -0,0 +1,22 @@
+package com.peng.demo.handler.security;
+
+import com.alibaba.fastjson.JSON;
+import com.peng.demo.common.ResponseResult;
+import com.peng.demo.util.WebUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationEntryPointImpl implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ResponseResult result = new ResponseResult(HttpStatus.FORBIDDEN.value(), "权限不足");
+        String json = JSON.toJSONString(result);
+        WebUtil.renderString(response,json);
+    }
+}
