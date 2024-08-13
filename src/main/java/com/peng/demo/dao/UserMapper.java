7da93ec4b0524099f02281c080f20b931a9e34a9
bigpeng <1015792002@qq.com>
2024-08-13 19:40:19 +0800

    commit

--- src/main/java/com/peng/demo/dao/UserMapper.java
+++ src/main/java/com/peng/demo/dao/UserMapper.java
@@ -2,6 +2,8 @@ package com.peng.demo.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.peng.demo.domain.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
---
+++ src/main/java/com/peng/demo/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.peng.demo.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.peng.demo.dao.UserMapper;
+import com.peng.demo.domain.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getUserName,username);
+        User user = userMapper.selectOne(wrapper);
+        //如果查询不到数据就通过抛出异常来给出提示
+        if(Objects.isNull(user)){
+            throw new RuntimeException("用户名或密码错误");
+        }
+        //TODO 根据用户查询权限信息 添加到LoginUser中
+
+        //封装成UserDetails对象返回
+        return new LoginUser(user);
+    }
+}
