412ab1ebb825f939a02d7e0792929834e2a4b3da
bigpeng <1015792002@qq.com>
2024-08-13 17:31:03 +0800

    commit

--- src/main/java/com/peng/demo/domain/entity/User.java
+++ src/main/java/com/peng/demo/domain/entity/User.java
@@ -1,4 +1,16 @@
 package com.peng.demo.domain.entity;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 用户表(User)实体类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
 }
