89752ae267ada3e10479920ee7e3d790786aa6c2
bigpeng <1015792002@qq.com>
2024-08-13 22:08:10 +0800

    commit

--- src/main/java/com/peng/demo/dao/UserMapper.java
+++ src/main/java/com/peng/demo/dao/UserMapper.java
@@ -2,8 +2,6 @@ package com.peng.demo.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.peng.demo.domain.entity.User;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
