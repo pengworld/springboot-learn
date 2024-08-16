74174baf76a79504246300fd82ef04382b2e470e
bigpeng <1015792002@qq.com>
2024-08-16 11:48:04 +0800

    commit

---
+++ src/main/java/com/peng/demo/handler/security/MyFailureHandler.java
@@ -0,0 +1,17 @@
+package com.peng.demo.handler.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        //认证失败器处理
+        System.out.println("认证失败");
+    }
+}
