9837b4daa378e4b55d260bffdebedc28f8499cbf
bigpeng <1015792002@qq.com>
2024-08-16 11:33:36 +0800

    commit

--- src/main/java/com/peng/demo/controller/TestController.java
+++ src/main/java/com/peng/demo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.peng.demo.controller;
 
 import com.peng.demo.handler.exception.GlobalExceptionHandler;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @RequestMapping("/home")
+    @PreAuthorize("@ex.hasAuthority('test')")
     public String home() throws Exception {
         throw new GlobalExceptionHandler("101", "Global Error 错误");
     }
