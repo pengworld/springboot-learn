ca9d6a70e165502a15a743cd2f09ce0be54728b6
bigpeng <1015792002@qq.com>
2024-08-17 11:44:58 +0800

    commit

--- src/main/java/com/peng/demo/annotation/OperateLog.java
+++ src/main/java/com/peng/demo/annotation/OperateLog.java
@@ -1,8 +1,11 @@
 package com.peng.demo.annotation;
 
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 //自定义注解  比如权限校验、日志记录、统计等非业务代码
-@Target({})
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface OperateLog {
+    String value() default "";
 }
---
+++ src/main/java/com/peng/demo/aspect/LogAspect.java
@@ -0,0 +1,10 @@
+package com.peng.demo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+}
