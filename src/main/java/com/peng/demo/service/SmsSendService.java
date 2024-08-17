7b5ac9f7fe3f6965f84da2da444f37f3d898c68a
bigpeng <1015792002@qq.com>
2024-08-17 21:16:35 +0800

    commit

---
+++ src/main/java/com/peng/demo/service/SmsSendService.java
@@ -0,0 +1,8 @@
+package com.peng.demo.service;
+
+/**
+ * 短信服务接口
+ * */
+public interface SmsSendService {
+    void smsSend();
+}
---
+++ src/main/java/com/peng/demo/service/impl/SmsSendServiceImpl.java
@@ -0,0 +1,12 @@
+package com.peng.demo.service.impl;
+
+import com.peng.demo.service.SmsSendService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsSendServiceImpl implements SmsSendService {
+    @Override
+    public void smsSend() {
+
+    }
+}
