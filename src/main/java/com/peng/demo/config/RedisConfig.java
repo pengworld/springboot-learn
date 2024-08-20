01be183aa94e5ea0345442d31541f09f4be85f78
bigpeng <1015792002@qq.com>
2024-08-20 11:47:26 +0800

    commit

--- src/main/java/com/peng/demo/config/RedisConfig.java
+++ src/main/java/com/peng/demo/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.peng.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -34,7 +35,7 @@ public class RedisConfig {
         rsc.setHostName("loalhost");
         rsc.setPort(6379);
         //redis安装后默认是没有密码的
-        //rsc.setPassword(RedisPassword.of("123"));
+        rsc.setPassword(RedisPassword.of("123"));
         this.connectionFactory = connectionFactory;
         return connectionFactory;
     }
